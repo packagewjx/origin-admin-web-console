f86296ea5a916448e366bbf5835d4b98991dfcf0
Wujunxian <wu812730157@gmail.com>
2018-03-21 15:37:06 +0800

    deleted welcome page words and build

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -15,8 +15,6 @@ class WelcomeView extends React.Component {
                             <p>
                                 请从左方导航菜单中进入需要的管理页
                                 <br/>
-                                本Web应用仍在开发中，如有功能不完善或是遇到了Bug，请提交issue到本应用的<a
-                                href="https://github.com/packagewjx/origin-admin-web-console/issues">github仓库</a>
                             </p>
                         </Col>
                     </Row>
